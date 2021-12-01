61859b19266eba4804a0492766777a134b65e0f4
Knijus <justinas.kniuksta@gmail.com>
2021-12-01 14:13:29 +0200

    Revert "unit test draft"
    This reverts commit 61ea9fdb41ab8cfd47a5ce4fb4b3da15d27b78ed.

--- Horse-racing Duals/index.js
+++ Horse-racing Duals/index.js
@@ -1,33 +1,27 @@
-function smallestDifference(input) {
-	const inputArray = input.split('/n');
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 
-	// Start of solution for puzzle
-	
-	let horses = [];
-	const N = parseInt(readline());
-	
-	for (let i = 0; i < N; i++) {
-		horses.push(parseInt(readline()));
-	}
-	   
-	horses.sort((a, b) => a-b);
-	
-	let smallestDiff = horses[1] - horses[0];
-	
-	for (let j = 2; j < N; j++){
-		const newDiff = horses[j] - horses[j-1];
-		if (newDiff < smallestDiff) {
-			smallestDiff = newDiff;
-		}
-	}
+ var horses = [];
 
-	return smallestDiff; // console.log() instead of return
-
-	// End of solution
-
-	function readline() {
-		return inputArray.shift();
-	}
-}
+ const N = parseInt(readline());
  
-module.exports = smallestDifference();
+ for (let i = 0; i < N; i++) {
+	 horses.push(parseInt(readline()));
+ }
+	
+ horses.sort((a, b) => a-b);
+ 
+ var smallestDiff = horses[1] - horses[0];
+ 
+ for (let j = 2; j < N; j++){
+	 var newDiff = horses[j] - horses[j-1];
+	 if (newDiff < smallestDiff) {
+		 smallestDiff = newDiff;
+	 }
+ }
+ // Write an answer using console.log()
+ // To debug: console.error('Debug messages...');
+ console.log(smallestDiff);
+ 
