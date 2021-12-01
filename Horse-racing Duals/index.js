3d992b3ad6160f5fb507b27fb2db5dba97ddeae7
Knijus <justinas.kniuksta@gmail.com>
2021-12-01 14:26:34 +0200

    unit test draft

--- Horse-racing Duals/index.js
+++ Horse-racing Duals/index.js
@@ -1,27 +1,31 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+function smallestDifference(input) {
+	const inputArray = input.split('/n');
 
- var horses = [];
-
- const N = parseInt(readline());
- 
- for (let i = 0; i < N; i++) {
-	 horses.push(parseInt(readline()));
- }
+	// Start of solution for puzzle
 	
- horses.sort((a, b) => a-b);
- 
- var smallestDiff = horses[1] - horses[0];
- 
- for (let j = 2; j < N; j++){
-	 var newDiff = horses[j] - horses[j-1];
-	 if (newDiff < smallestDiff) {
-		 smallestDiff = newDiff;
-	 }
- }
- // Write an answer using console.log()
- // To debug: console.error('Debug messages...');
- console.log(smallestDiff);
- 
+	let horses = [];
+	const N = parseInt(readline());
+
+	for (let i = 0; i < N; i++) {
+		horses.push(parseInt(readline()));
+	}
+
+	horses.sort((a, b) => a-b);
+
+	let smallestDiff = horses[1] - horses[0];
+
+	for (let j = 2; j < N; j++){
+		const newDiff = horses[j] - horses[j-1];
+		if (newDiff < smallestDiff) {
+			smallestDiff = newDiff;
+		}
+	}
+
+	return smallestDiff; // console.log() instead of return
+
+	// End of solution
+
+	function readline() {
+		return inputArray.shift();
+	}
+}
