1dedc7fd9e84bba3c2cd9efad90aea0c97c63dc2
Knijus <justinas.kniuksta@gmail.com>
2021-12-02 10:08:06 +0200

    unit test for horse-racing duals

--- Horse-racing Duals/index.test.js
+++ Horse-racing Duals/index.test.js
@@ -1,12 +1,17 @@
 const index = require('./index');
 const fs = require('fs');
-const simpleCase = fs.readFileSync('./simpleCase.txt', 'utf-8');
-const horseInAnyOrder = fs.readFileSync('./horsesInAnyOrder.txt', 'utf-8');
+const simpleCase = fs.readFileSync('./testCaseScenarios/simpleCase.txt', 'utf-8');
+const horseInAnyOrder = fs.readFileSync('./testCaseScenarios/horsesInAnyOrder.txt', 'utf-8');
+const manyHorses = fs.readFileSync('./testCaseScenarios/manyHorses.txt', 'utf-8');
 
-test('Simple case', () => {
+test('Simple case. Smallest difference is 1', () => {
 	expect(index(simpleCase)).toBe(1);
   });
 
-  test('Horses in any order', () => {
+  test('Horses in any order. Smallest difference is 1', () => {
 	expect(index(horseInAnyOrder)).toBe(1);
+  });
+
+  test('Many horses. Smallest difference is 47', () => {
+	expect(index(manyHorses)).toBe(47);
   });
