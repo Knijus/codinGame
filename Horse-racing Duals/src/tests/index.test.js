30afa364f8e145b5fd0f553f7f1c2c3ffaa3159f
Knijus <justinas.kniuksta@gmail.com>
2021-12-02 13:01:49 +0200

    updated unit test

--- Horse-racing Duals/src/tests/index.test.js
+++ Horse-racing Duals/src/tests/index.test.js
@@ -1,20 +1,21 @@
 const testSubject = require('../index');
 const fs = require('fs');
+const path = require("path");
 
 test('Simple case. Smallest difference is 1', () => {
-  const input = fs.readFileSync('./simpleCase.txt', 'utf-8');
+  const input = fs.readFileSync(path.join(__dirname, "simpleCase.txt"), 'utf-8');
   const output = testSubject(input);
 	expect(output).toBe(1);
 });
 
 test('Horses in any order. Smallest difference is 1', () => {
-  const input = fs.readFileSync('./horsesInAnyOrder.txt', 'utf-8');
+  const input = fs.readFileSync(path.join(__dirname, "horsesInAnyOrder.txt"), 'utf-8');
   const output = testSubject(input);
   expect(output).toBe(1);
 });
 
 test('Many horses. Smallest difference is 47', () => {
-  const input = fs.readFileSync('./manyHorses.txt', 'utf-8');
+  const input = fs.readFileSync(path.join(__dirname, "manyHorses.txt"), 'utf-8');
   const output = testSubject(input);
   expect(output).toBe(47);
 });
