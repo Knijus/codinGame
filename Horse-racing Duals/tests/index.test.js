538bac8278e257453b75942f733c442377e4ffe1
Knijus <justinas.kniuksta@gmail.com>
2021-12-09 12:05:09 +0200

    updated tests with mock function

--- Horse-racing Duals/tests/index.test.js
+++ Horse-racing Duals/tests/index.test.js
@@ -1,21 +1,32 @@
-const testSubject = require('../src/index');
-const fs = require('fs');
-const path = require("path");
+describe("Index.js function",() => {
+  const testSubject = require('../src/index');
+  const fs = require('fs');
+  const path = require("path");
 
-test('Simple case. Smallest difference is 1', () => {
-  const input = fs.readFileSync(path.join(__dirname, "simpleCase.txt"), 'utf-8');
-  const output = testSubject(input);
-	expect(output).toBe(1);
+  beforeAll(() => {
+    console.log = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('Simple case. Smallest difference is 1', () => {
+    const input = fs.readFileSync(path.join(__dirname, "simpleCase.txt"), 'utf-8');
+    testSubject(input);
+    expect(console.log).toBeCalledWith(1);
+  });
+
+  test('Horses in any order. Smallest difference is 1', () => {
+    const input = fs.readFileSync(path.join(__dirname, "horsesInAnyOrder.txt"), 'utf-8');
+    testSubject(input);
+    expect(console.log).toBeCalledWith(1);
+  });
+
+  test('Many horses. Smallest difference is 47', () => {
+    const input = fs.readFileSync(path.join(__dirname, "manyHorses.txt"), 'utf-8');
+    testSubject(input);
+    expect(console.log).toBeCalledWith(47);
+  });
 });
 
-test('Horses in any order. Smallest difference is 1', () => {
-  const input = fs.readFileSync(path.join(__dirname, "horsesInAnyOrder.txt"), 'utf-8');
-  const output = testSubject(input);
-  expect(output).toBe(1);
-});
-
-test('Many horses. Smallest difference is 47', () => {
-  const input = fs.readFileSync(path.join(__dirname, "manyHorses.txt"), 'utf-8');
-  const output = testSubject(input);
-  expect(output).toBe(47);
-});
